168b2a69cd44eb11bb0d245ca714fd4133da79f7
mikeselander <mikeselander@humanmade.co.uk>
2018-01-18 13:41:58 -0800

    Add Points of Contact block and story for the block

---
+++ src/components/Cloud/Points-Of-Contact.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import DashboardBlock from '../Dashboard-Block';
+
+/**
+ * Display email addresses for important points of contact for the site.
+ *
+ * @param {Array} contacts C
+ */
+const PointsOfContact = ( { contacts } ) => <DashboardBlock title="Internal Point of Contact" id="points-of-contact">
+	<div className="points-of-contact">
+
+		{ contacts.map( item => {
+			return <div className="points-of-contact__section">
+				<h3 className="points-of-contact__title">{ item.title }</h3>
+
+				{ item.emails.map( email => <a className="points-of-contact__email" href={ `mailto:${ email }` }>{ email }</a> ) }
+			 </div>
+		} ) }
+	</div>
+</DashboardBlock>
+
+PointsOfContact.defaultProps = { contact: [] }
+
+PointsOfContact.propTypes = {
+	contacts: PropTypes.arrayOf( [
+		PropTypes.shape( {
+			title:  PropTypes.string,
+			emails: PropTypes.arrayOf( [ PropTypes.string ] ),
+		} ),
+	] ),
+}
+
+export default PointsOfContact;
