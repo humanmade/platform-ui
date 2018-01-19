91872766b7f1c9286150e0ed0aeaca237858e33f
mikeselander <mikeselander@humanmade.co.uk>
2018-01-19 13:34:54 -0800

    Add loading awareness to Dashboard Block

--- src/components/Dashboard-Block.js
+++ src/components/Dashboard-Block.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 /**
  * Generic Dashboard wrapper block for displaying some information in the admin.
  *
- * @param {String} title   Title to display in collapsbile header.
- * @param {Array} children React children.
+ * @param {Boolean} isLoading Whether Data is loading
+ * @param {String}  title     Title to display in collapsbile header.
+ * @param {Array}   children  React children.
  */
-const DashboardBlock = ( { title, children } ) => {
+const DashboardBlock = ( { isLoading, title, children } ) => {
 	return <div className="meta-box-sortables">
 		<div className="postbox">
 			<button type="button" className="handlediv" aria-expanded="true">
@@ -15,13 +16,14 @@ const DashboardBlock = ( { title, children } ) => {
 				<span className="toggle-indicator" aria-hidden="true" />
 			</button>
 			<h2 className="hndle"><span>{title}</span></h2>
-			<div className="inside">
+			<div className={ `inside ${ isLoading ? ' dashboard-block--loading' : '' }` }>
 				{children}
 			</div>
 		</div>
 	</div>
 }
 
+DashboardBlock.defaultProps = { isLoading: true }
 DashboardBlock.propTypes = { title: PropTypes.string }
 
 export default DashboardBlock;
