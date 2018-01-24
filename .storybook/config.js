3c9981da247e3f6ff45f3039e39c2489d5e0ae28
CircleCI <rob+platform-ui-build@hmn.md>
2018-01-24 18:59:37 +0000

    Build changes from 7a30038ebd3cb4c9bfb4bb271c2185a826b05c6c
    https://circleci.com/gh/humanmade/platform-ui/8

---
+++ .storybook/config.js
@@ -0,0 +1,22 @@
+import { configure } from '@storybook/react';
+
+// Import WordPress admin-side styles.
+import '../../../../../wordpress/wp-admin/css/admin-menu.css';
+import '../../../../../wordpress/wp-admin/css/common.css';
+import '../../../../../wordpress/wp-admin/css/dashboard.css';
+import '../../../../../wordpress/wp-admin/css/forms.css';
+import '../../../../../wordpress/wp-admin/css/widgets.css';
+import '../../../../../wordpress/wp-admin/css/wp-admin.css';
+import '../../../../../wordpress/wp-includes/css/buttons.css';
+
+
+// Our Application's CSS.
+import '../src/index.css';
+
+const req = require.context('../src/stories', true, /\.stories\.js$/)
+
+function loadStories() {
+	req.keys().forEach( ( filename ) => req( filename ) )
+}
+
+configure( loadStories, module );
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require( 'path' );
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /.(png|gif|jpg|jpeg|svg)(\?[a-z0-9=.]+)?$/,
+				loader: 'url-loader?limit=100000'
+			},
+			{
+				test: /.(scss|css)$/,
+				loaders: [ 'style-loader', 'css-loader' ],
+				include: [
+					path.resolve( __dirname, '../../../../../wordpress/wp-admin/css' ),
+					path.resolve( __dirname, '../../../../../wordpress/wp-includes/css' ),
+					path.resolve( __dirname, '../src' ),
+				]
+			},
+		],
+	},
+};
