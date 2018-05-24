25bcc3d6838b4dd5f633a7942ff69e1e9a29f3f6
CircleCI <rob+platform-ui-build@hmn.md>
2018-05-24 20:34:30 +0000

    Build changes from 2d2f29e1f22314d6884490f40a88f66742675351
    https://circleci.com/gh/humanmade/platform-ui/113

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-// eslint-disable-next-line
-/*global HM*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-/**
- * @todo fix service worker cache when multiple instances present
- */
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render( <App />, document.getElementById( 'hm-platform-root' ) );
-//registerServiceWorker();
+registerServiceWorker();
