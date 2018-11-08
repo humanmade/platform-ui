fbc466bd05a556df7d105f39ab67940a9fa3d623
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-08 15:05:58 +0800

    PRs: Use StatusIndicator

--- src/components/Cloud/Pull-Request-Item.js
+++ src/components/Cloud/Pull-Request-Item.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { distanceInWordsToNow } from 'date-fns';
 
+import StatusIndicator from '../StatusIndicator';
+
 /**
  * Display status about a single Pull Request.
  *
@@ -12,7 +14,7 @@ import { distanceInWordsToNow } from 'date-fns';
  * @param {String} statusText Formatted string describing the current status of the pull request,
  * @param {String} title      Title of the pull request.
  */
-const PullRequestItem = ( { date, id, link, status, statusText, title } ) => {
+const PullRequestItem = ( { date, id, link, status, title } ) => {
 	const parsedDate = new Date( date );
 	const dateTime = parsedDate.toISOString();
 	const dateString = distanceInWordsToNow( parsedDate, { addSuffix: true } );
@@ -24,10 +26,12 @@ const PullRequestItem = ( { date, id, link, status, statusText, title } ) => {
 			</h3>
 			<time datetime={ dateTime } className="pull-request-item__date">{ dateString }</time>
 		</div>
-		<div className="pull-request-item__status">
-			<div className={ `pull-request-item__status-indicator pr-status--${ status }` } />
-			{ statusText }
-		</div>
+		<StatusIndicator
+			className="pull-request-item__status"
+			status={ 'commented' }
+		>
+			{ status }
+		</StatusIndicator>
 	</li>
 }
 
