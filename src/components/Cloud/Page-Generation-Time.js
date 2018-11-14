2243bce5fa94ecd653961940ee6a2d6d3cd89162
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-14 21:21:15 +0800

    PageGenerationTime: Use DashboardBlock.withData()

--- src/components/Cloud/Page-Generation-Time.js
+++ src/components/Cloud/Page-Generation-Time.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import withApiFetch from '../../utils/withApiFetch';
-import orWpError from '../../utils/wp-error';
-import { compose } from 'recompose';
 import { VictoryLine, VictoryChart, VictoryAxis } from 'victory';
 
-import DashboardBlock from '../Dashboard-Block';
+import orWpError from '../../utils/wp-error';
+import { withData } from '../Dashboard-Block';
 import { adminTheme } from '../../victory-theme';
 
 /**
@@ -14,10 +12,10 @@ import { adminTheme } from '../../victory-theme';
  * @param {Boolean} loading Whether data is still fetching or not.
  * @param {Array}   data    An array of server response data for the current site.
  */
-const PageGenerationTime = ( { loading, data } ) => {
+const PageGenerationTime = ( { data } ) => {
 
-	if ( loading || ! Array.isArray( data ) ) {
-		return '';
+	if ( ! Array.isArray( data ) || ! data.length ) {
+		return <p>No data found.</p>;
 	}
 
 	/**
@@ -32,34 +30,29 @@ const PageGenerationTime = ( { loading, data } ) => {
 	const highestTime = data.reduce( ( acc, datum ) => datum.value > acc ? datum.value : acc, 0 );
 
 	return (
-		<DashboardBlock id="cloud-page-gen-time-block" title="Page Generation Time" isLoading={ loading }>
-			<VictoryChart
-				theme={ adminTheme }
-				domainPadding={ 10 }
-			>
-				<VictoryAxis
-					dependentAxis
-					tickCount={ 5 }
-					tickFormat={ formatLabels }
-				/>
-				<VictoryAxis
-					label="(Date)"
-					tickCount={ 7 }
-					tickFormat={ x => new Date( x ).getDate() }
-					style={ { grid: {
-							fill: "none",
-							stroke: "none",
-							pointerEvents: "visible"
-						} } }
-				/>
-				<VictoryLine
-					data={ data }
-					labels={ datum => datum.value > ( highestTime * .8 ) ? formatLabels( datum.value ) : '' }
-					x="date"
-					y="value"
-				/>
-			</VictoryChart>
-		</DashboardBlock>
+		<VictoryChart theme={ adminTheme } domainPadding={ 10 }>
+			<VictoryAxis
+				dependentAxis
+				tickCount={ 5 }
+				tickFormat={ formatLabels }
+			/>
+			<VictoryAxis
+				label="(Date)"
+				tickCount={ 7 }
+				tickFormat={ x => new Date( x ).getDate() }
+				style={ { grid: {
+						fill: "none",
+						stroke: "none",
+						pointerEvents: "visible"
+					} } }
+			/>
+			<VictoryLine
+				data={ data }
+				labels={ datum => datum.value > ( highestTime * .8 ) ? formatLabels( datum.value ) : '' }
+				x="date"
+				y="value"
+			/>
+		</VictoryChart>
 	);
 }
 
@@ -78,8 +71,10 @@ PageGenerationTime.propTypes = {
 	loading: PropTypes.bool
 };
 
-const PageGenerationTimeWithData = compose(
-	withApiFetch( 'hm-stack/v1/page-generation/' )
-)( PageGenerationTime );
+const PageGenerationTimeWithData = withData( {
+	url: 'hm-stack/v1/page-generation/',
+	id: 'cloud-page-gen-time-block',
+	title: 'Page Generation Time',
+} )( PageGenerationTime );
 
 export default PageGenerationTimeWithData;
