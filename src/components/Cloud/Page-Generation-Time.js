00d45096cf8d637781a50e42aa0efd557e769ad4
Dzikri Aziz <kvcrvt@gmail.com>
2018-11-15 13:16:29 +0800

    PageGenerationTime: Use LoadableVictory

--- src/components/Cloud/Page-Generation-Time.js
+++ src/components/Cloud/Page-Generation-Time.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { VictoryLine, VictoryChart, VictoryAxis } from 'victory';
 
 import orWpError from '../../utils/wp-error';
 import { withData } from '../Dashboard-Block';
-import { adminTheme } from '../../victory-theme';
+import LoadableVictory from '../LoadableVictory';
 
 /**
  * Display daily average page generation time for a rolling month period.
@@ -12,11 +11,13 @@ import { adminTheme } from '../../victory-theme';
  * @param {Boolean} loading Whether data is still fetching or not.
  * @param {Array}   data    An array of server response data for the current site.
  */
-const PageGenerationTime = ( { data } ) => {
-
+const PageGenerationTime = ( { victory, data } ) => {
 	if ( ! Array.isArray( data ) || ! data.length ) {
 		return <p>No data found.</p>;
 	}
+
+	const { components, theme } = victory;
+	const { VictoryLine, VictoryChart, VictoryAxis } = components;
 
 	/**
 	 * Format a label for a time measuments.
@@ -30,7 +31,7 @@ const PageGenerationTime = ( { data } ) => {
 	const highestTime = data.reduce( ( acc, datum ) => datum.value > acc ? datum.value : acc, 0 );
 
 	return (
-		<VictoryChart theme={ adminTheme } domainPadding={ 10 }>
+		<VictoryChart theme={ theme } domainPadding={ 10 }>
 			<VictoryAxis
 				dependentAxis
 				tickCount={ 5 }
@@ -75,6 +76,6 @@ const PageGenerationTimeWithData = withData( {
 	url: 'hm-stack/v1/page-generation/',
 	id: 'cloud-page-gen-time-block',
 	title: 'Page Generation Time',
-} )( PageGenerationTime );
+} )( LoadableVictory( PageGenerationTime ) );
 
 export default PageGenerationTimeWithData;
